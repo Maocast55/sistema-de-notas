63a00e83f138a5946938da9cecdf21cdcc7f13f5
Alumno <alumno@huayra.(none)>
2015-08-11 17:55:42 -0300

    Pantalla profesor 1 en proceso

--- sistemaDeNotas/core/views.py
+++ sistemaDeNotas/core/views.py
@@ -67,3 +67,7 @@ class LogOutView(View):
     def get(self, request):
         logout(request)
         return redirect('login')
+
+class ProfesorView(View):
+    def get(self, request):
+       return render(request, 'pantalla_profesor.html')

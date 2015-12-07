16811e4915a6994c980fc298c82ad491f4dd5e1a
Morellato Ariel <morellatoariel@gmail.com>
2015-12-07 14:18:06 -0300

    Filtro por curso

--- sistemaDeNotas/core/management/commands/actualizar_trimestres.py
+++ sistemaDeNotas/core/management/commands/actualizar_trimestres.py
@@ -5,8 +5,9 @@ from core.views import actualizar_promedios
 
 class Command(BaseCommand):
 
+    # args [0] -> cursada . Ej: 1, 2, 3, 4, 5, 6, 7
     def handle(self, *args, **options):
-        materias = Materia.objects.all()
+        materias = Materia.objects.filter(seccion__cursada=args[0])
         for materia in materias:
             print "actualizando ... " + materia.nombre + ' | ' + str(materia.seccion)
             alumnos_inscriptos = map(lambda i: i.alumno, filter(lambda i : i.fecha_baja == None or i.fecha_baja> datetime.datetime.now().date(), Inscripcion.objects.filter(seccion=materia.seccion)))
@@ -15,5 +16,8 @@ class Command(BaseCommand):
                 for trimestre in trimestres:
                     #print "actualizando ... " + str(alumno.dni) + ' | ' + materia.nombre + ' | ' + str(trimestre)
                     actualizar_promedios(alumno, materia, trimestre)
+
+    def add_arguments(self, parser):
+        parser.add_argument('args')
 
 
